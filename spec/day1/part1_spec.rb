07a6c235d8fb320f8172d3f014a00132ed3ed03b
Lissa Moriarty <njm506@york.ac.uk>
2023-12-01 17:50:18 +0000

    Fix cops

--- spec/day1/part1_spec.rb
+++ spec/day1/part1_spec.rb
@@ -1,41 +1,35 @@
 # frozen_string_literal: true
 
-require 'day1/part2'
+require 'day1/part1'
 
-RSpec.describe Day1::Part2 do
+RSpec.describe Day1::Part1 do
   subject(:p1) { described_class.new }
 
-  let(:example_data) do
-    %w[two1nine eightwothree abcone2threexyz xtwone3four
-       4nineeightseven2 zoneight234 7pqrstsixteen]
-  end
+  let(:example_data) { %w[1abc2 pqr3stu8vwx a1b2c3d4e5f treb7uchet] }
 
   describe '.calibration_value' do
     it 'meets the given examples', :aggregate_failures do
-      expected_results = [29, 83, 13, 24, 42, 14, 76]
+      expected_results = [12, 38, 15, 77]
 
       example_data.each_with_index do |s, i|
         expect(p1.calibration_value(s)).to eq(expected_results[i])
       end
     end
-
-    it 'extracts correctly from a line with overlapping words' do
-      expect(p1.calibration_value('8fourfouroneightr')).to eq(88)
-    end
   end
 
   describe '.sum' do
     it 'meets the given example' do
-      expect(p1.sum(example_data.each)).to eq(281)
+      expect(p1.sum(example_data.each)).to eq(142)
     end
   end
 
   context 'when given an input file' do
     subject(:the_answer) { p1.sum(input_lines) }
+
     with_input('1/input')
 
     it { is_expected.to be_a(Numeric) }
 
-    it { is_expected.to eq(55218) }
+    it { is_expected.to eq(54951) }
   end
 end
