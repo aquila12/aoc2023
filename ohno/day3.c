9435e0022258e3a11ef35f9076255df7737e6d87
Lissa Moriarty <aquila12@foxtails.uk>
2023-12-03 20:42:02 +0000

    Rehash day 3 implementation to reduce branching

--- ohno/day3.c
+++ ohno/day3.c
@@ -11,58 +11,67 @@
 
 static const size_t mmap_len = 65536;
 
-struct thing {
-  enum thing_type { NUMBER, SYMBOL } type;
-  union {
-    struct thing_sym { int col; char symbol; int ratio; int nums_adj; } sym;
-    struct thing_num { int col0, col1, value; } num;
-  };
-};
+struct thing_sym { int col; char symbol; int ratio; int nums_adj; } sym;
+struct thing_num { int col0, col1, value; } num;
 
 struct row {
-  int n_things;
-  struct thing things[100];
+  int n_syms, n_nums;
+  struct thing_sym symbols[50];
+  struct thing_num numbers[50];
 };
 
-static int found_node(
-  struct thing *thing, struct row *this_row, struct row *last_row,
-  int (*found_adjacent)(struct thing *, struct thing *)
+inline static int found_num(
+  struct thing_num *num, struct row *this_row, struct row *last_row,
+  int (*found_adjacent)(struct thing_num *, struct thing_sym *)
 ) {
   int sum = 0;
-  int num = thing->type == NUMBER;
+  int lim = last_row->n_syms;
 
-  for(int n = 0; n<last_row->n_things; ++n) {
-    struct thing *other = &last_row->things[n];
-    if(num) {
-      if(other->type == SYMBOL) {
-        if(other->sym.col > thing->num.col1) break;
-        if(other->sym.col >= thing->num.col0) sum += found_adjacent(thing, other);
-      }
-    } else {
-      if(other->type == NUMBER) {
-        if(other->num.col0 > thing->sym.col) break;
-        if(other->num.col1 >= thing->sym.col) sum += found_adjacent(other, thing);
-      }
-    }
+  for(int n = 0; n < lim; ++n) {
+    struct thing_sym *sym = &last_row->symbols[n];
+
+    if(sym->col < num->col0) continue;
+    if(sym->col > num->col1) break;
+    sum += found_adjacent(num, sym);
   }
 
-  int t = this_row->n_things;
-  if(t > 1) {
-    struct thing *other = &this_row->things[t - 2];
-    if(num) {
-      if(other->type == SYMBOL && other->sym.col == thing->num.col0)
-        sum += found_adjacent(thing, other);
-    } else {
-      if(other->type == NUMBER && thing->sym.col == other->num.col1)
-        sum += found_adjacent(other, thing);
-    }
+  int t = this_row->n_syms;
+  if(t) {
+    struct thing_sym *sym = &this_row->symbols[t - 1];
+    if(sym->col == num->col0)
+      sum += found_adjacent(num, sym);
+  }
+
+  return sum;
+}
+
+inline static int found_sym(
+  struct thing_sym *sym, struct row *this_row, struct row *last_row,
+  int (*found_adjacent)(struct thing_num *, struct thing_sym *)
+) {
+  int sum = 0;
+  int lim = last_row->n_nums;
+
+  for(int n = 0; n < lim; ++n) {
+    struct thing_num *num = &last_row->numbers[n];
+
+    if(num->col1 < sym->col) continue;
+    if(num->col0 > sym->col) break;
+    sum += found_adjacent(num, sym);
+  }
+
+  int t = this_row->n_nums;
+  if(t) {
+    struct thing_num *num = &this_row->numbers[t - 1];
+    if(num->col1 == sym->col)
+      sum += found_adjacent(num, sym);
   }
 
   return sum;
 }
 
 static int parser(
-  int (*found_adjacent)(struct thing *, struct thing *),
+  int (*found_adjacent)(struct thing_num *, struct thing_sym *),
   int (*after_row)(struct row *)
 ) {
   int sum = 0;
@@ -77,54 +86,51 @@ static int parser(
   if(buf == MAP_FAILED) { sum = -3; goto _close; }
 
   int i = 0;
-  int width, numeral = 0, col;
+  int width, numeral = 0, col, col0;
 
   // Calibrate input - assume constant width (well-behaved input)
   for(width = 0; width < fs.st_size && buf[width] != '\n'; ++width);
 
-  struct row row_a = { .n_things = 0 }, row_b = { .n_things = 0 };
+  struct row row_a = { .n_nums = 0, .n_syms = 0 }, row_b = { .n_nums = 0, .n_syms = 0 };
   struct row *this_row = &row_a, *last_row = &row_b, *tmp_row;
-
-  struct thing *this_thing;
 
   while(i < fs.st_size) {
     tmp_row = last_row;
     last_row = this_row;
     this_row = tmp_row;
 
-    this_row->n_things = 0;
+    this_row->n_nums = 0;
+    this_row->n_syms = 0;
     col = 0;
 
     while(i < fs.st_size) {
       char c = buf[i++];
       if(c >= '0' && c <= '9') {
-        if(!numeral) {
-          this_thing = &this_row->things[this_row->n_things];
-          this_thing->type = NUMBER;
-          this_thing->num.col0 = col - 1;
-        } else numeral *= 10;
+        if(!numeral) col0 = col - 1;
+        else numeral *= 10;
 
         numeral += c - '0';
       } else {
         if(numeral) {
-          this_thing->num.col1 = col;
-          this_thing->num.value = numeral;
-          ++this_row->n_things;
+          struct thing_num *num = &this_row->numbers[this_row->n_nums];
+          num->col0 = col0;
+          num->col1 = col;
+          num->value = numeral;
+          ++this_row->n_nums;
           numeral = 0;
-          sum += found_node(this_thing, this_row, last_row, found_adjacent);
+          sum += found_num(num, this_row, last_row, found_adjacent);
         }
 
         if(c == '\n') break; /* EOL Break clause */
 
         if(c != '.') {
-          this_thing = &this_row->things[this_row->n_things];
-          this_thing->type = SYMBOL;
-          this_thing->sym.col = col;
-          this_thing->sym.symbol = c;
-          this_thing->sym.nums_adj = 0;
-          this_thing->sym.ratio = 0;
-          ++this_row->n_things;
-          sum += found_node(this_thing, this_row, last_row, found_adjacent);
+          struct thing_sym *sym = &this_row->symbols[this_row->n_syms];
+          sym->col = col;
+          sym->symbol = c;
+          sym->nums_adj = 0;
+          sym->ratio = 0;
+          ++this_row->n_syms;
+          sum += found_sym(sym, this_row, last_row, found_adjacent);
         }
       }
       ++col;
@@ -144,13 +150,13 @@ static int parser(
 
 
 /* NB This only works because no number is adjacent to two symbols */
-static int found_adjacent_1(struct thing *num, struct thing *sym) {
+static int found_adjacent_1(struct thing_num *num, struct thing_sym *sym) {
   // printf(
   //   "%2d %c %2d %2d %d\t",
   //   sym->sym.col, sym->sym.symbol,
   //   num->num.col0, num->num.col1, num->num.value
   // );
-  return num->num.value;
+  return num->value;
 }
 
 static int after_row_1(struct row *_) { return 0; }
@@ -159,13 +165,13 @@ static int part1() {
   return parser(found_adjacent_1, after_row_1);
 }
 
-static int found_adjacent_2(struct thing *num, struct thing *sym) {
-  if(sym->sym.symbol != '*') return 0;
+static int found_adjacent_2(struct thing_num *num, struct thing_sym *sym) {
+  if(sym->symbol != '*') return 0;
 
-  ++sym->sym.nums_adj;
+  ++sym->nums_adj;
 
-  if(sym->sym.nums_adj == 1) sym->sym.ratio = num->num.value;
-  else if(sym->sym.nums_adj == 2) sym->sym.ratio *= num->num.value;
+  if(sym->nums_adj == 1) sym->ratio = num->value;
+  else if(sym->nums_adj == 2) sym->ratio *= num->value;
 
   return 0;
 }
@@ -173,15 +179,13 @@ static int found_adjacent_2(struct thing *num, struct thing *sym) {
 static int after_row_2(struct row *row) {
   int sum = 0;
 
-  for(int n=0; n < row->n_things; ++n) {
-    struct thing *thing = &row->things[n];
+  for(int n=0; n < row->n_syms; ++n) {
+    struct thing_sym *sym = &row->symbols[n];
 
-    if(thing->type != SYMBOL) continue;
-    if(thing->sym.symbol != '*') continue;
-    // printf("* %d %d\t", thing->sym.nums_adj, thing->sym.ratio);
-    if(thing->sym.nums_adj != 2) continue;
+    if(sym->symbol != '*') continue;
+    if(sym->nums_adj != 2) continue;
 
-    sum += thing->sym.ratio;
+    sum += sym->ratio;
   }
 
   // printf("\n");
