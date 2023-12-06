01d7e3b986111393783762e58bba4d0c081ae410
Lissa Moriarty <aquila12@foxtails.uk>
2023-12-06 17:09:27 +0000

    Example for d6p1

---
+++ lib/day6/part1.rb
@@ -0,0 +1,65 @@
+# frozen_string_literal: true
+
+module Day6
+  # Work out how long to hold the button for
+  class Part1
+    # Simple helper class to hold race info
+    class Race
+      def initialize(time, distance)
+        @time = time
+        @distance = distance
+      end
+
+      attr_reader :time, :distance
+
+      def winning_times
+        # d = run_time * speed
+        # speed = hold_time
+        # run_time = time - hold_time
+        #
+        # d = (time - hold_time) * hold_time
+        # d = time * hold_time - hold_time²
+        # time * hold_time - hold_time² - d = 0
+        # ax² + bx + c = 0
+        # a = -1, b = time, c = -d
+        min, max = solve_quadratic(-1, time, -distance)
+        ((min + 1).floor)..((max - 1).ceil)
+      end
+
+      def margin
+        winning_times.size
+      end
+
+      def solve_quadratic(a, b, c) # rubocop:disable Naming/MethodParameterName
+        sqrt_term = Math.sqrt((b * b) - (4 * a * c))
+        over_2a = 1.0 / (2 * a)
+        x0 = (-b + sqrt_term) * over_2a
+        x1 = (-b - sqrt_term) * over_2a
+        x0 < x1 ? [x0, x1] : [x1, x0]
+      end
+    end
+
+    def initialize(data)
+      inputs = data.to_h do |l|
+        k, s = l.split(':', 2)
+        [k, s.split.map(&:to_i)]
+      end
+
+      @races = inputs['Time'].zip(inputs['Distance']).map do |t, d|
+        Race.new(t, d)
+      end
+    end
+
+    def race(number)
+      @races[number - 1]
+    end
+
+    def product
+      @races.map(&:margin).inject(:*)
+    end
+
+    def self.result(file)
+      new(file.each_line).product
+    end
+  end
+end
---
+++ spec/day6/part1_spec.rb
@@ -0,0 +1,59 @@
+# frozen_string_literal: true
+
+require 'day6/part1'
+
+RSpec.describe Day6::Part1 do
+  context 'with the example data', :aggregate_failures do
+    subject(:p1) { described_class.new(example_data.each) }
+
+    with_example_data :day6
+
+    describe 'the first race' do
+      subject(:race) { p1.race(1) }
+
+      example 'the inputs are correct' do
+        expect(race.time).to eq 7
+        expect(race.distance).to eq 9
+      end
+
+      example 'the winning times are correct' do
+        expect(race.winning_times).to eq 2..5
+        expect(race.margin).to eq 4
+      end
+    end
+
+    describe 'the second race' do
+      subject(:race) { p1.race(2) }
+
+      example 'the winning times are correct' do
+        expect(race.winning_times).to eq 4..11
+        expect(race.margin).to eq 8
+      end
+    end
+
+    describe 'the third race' do
+      subject(:race) { p1.race(3) }
+
+      example 'the winning times are correct' do
+        expect(race.winning_times).to eq 11..19
+        expect(race.margin).to eq 9
+      end
+    end
+
+    it 'produces the correct product' do
+      expect(p1.product).to eq 288
+    end
+  end
+
+  context 'with the input file' do
+    subject(:p1) { described_class.new(input_file.each_line) }
+
+    with_input :day6
+
+    describe '.product' do
+      subject(:product) { p1.product }
+
+      it { is_expected.to be_a Numeric }
+    end
+  end
+end
