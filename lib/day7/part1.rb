8ae5e91a3fee5458f3968cce7458dcfde0c933f2
Lissa Moriarty <njm506@york.ac.uk>
2023-12-07 17:15:49 +0000

    Day 7 part 1 implementation

---
+++ lib/day7/part1.rb
@@ -0,0 +1,57 @@
+# frozen_string_literal: true
+
+module Day7
+  # Camel cards implementation
+  class Part1
+    # Representation of a hand of cards with a bid
+    class Hand
+      LABELS = 'AKQJT98765432'
+      VALUES = 'abcdefghijklm'.reverse
+
+      def initialize(string, bid)
+        @cards = string.tr(LABELS, VALUES)
+        @bid = bid.to_i
+      end
+
+      def to_s
+        @cards.tr(VALUES, LABELS)
+      end
+
+      attr_reader :cards, :bid
+
+      include Comparable
+
+      def <=>(other)
+        _compare(type, other.type) || _compare(cards, other.cards) || 0
+      end
+
+      def type
+        return @type if @type
+
+        counts = @cards.chars.group_by(&:itself).values.map(&:length).sort
+        @type = (counts[-1] << 2) + (counts[-2] == 2 ? 1 : 0)
+      end
+
+      def _compare(one, two)
+        x = one <=> two
+        x.zero? ? nil : x
+      end
+    end
+
+    def initialize(lines)
+      @hands = lines.each.map do |l|
+        hand, bid = l.split
+        Hand.new(hand, bid)
+      end
+    end
+
+    def score
+      @hands.sort.each.with_index.sum do |hand, i|
+        rank = 1 + i
+        rank * hand.bid
+      end
+    end
+
+    attr_reader :hands
+  end
+end
---
+++ spec/day7/part1_spec.rb
@@ -0,0 +1,43 @@
+# frozen_string_literal: true
+
+require 'day7/part1'
+
+RSpec.describe Day7::Part1 do
+  context 'with the example data', :aggregate_failures do
+    subject(:p1) { described_class.new(example_data.each) }
+
+    with_example_data :day7
+
+    describe '.hands' do
+      subject(:hands) { p1.hands }
+
+      example 'there are five hands' do
+        expect(hands.length).to eq 5
+      end
+
+      example 'the hands sort correctly' do
+        expect(hands.sort.map(&:to_s)).to eq(%w[32T3K KTJJT KK677 T55J5 QQQJA])
+      end
+
+      example 'the bid amounts are correct' do
+        expect(hands.map(&:bid)).to eq([765, 684, 28, 220, 483])
+      end
+    end
+
+    it 'has the correct score' do
+      expect(p1.score).to eq 6440
+    end
+  end
+
+  context 'with the input file' do
+    subject(:p1) { described_class.new(input_file.each_line) }
+
+    with_input :day7
+
+    describe '.score' do
+      subject(:score) { p1.score }
+
+      it { is_expected.to be_a Numeric }
+    end
+  end
+end
