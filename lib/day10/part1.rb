b64cd5f595443586c2e9ca3a180835ac43511762
Lissa Moriarty <aquila12@foxtails.uk>
2023-12-10 12:30:59 +0000

    Draft day 10 part 1 solution

---
+++ lib/day10/part1.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+module Day10
+  class Part1
+    # N S W E
+    DIRECTIONS = [[-1, 0], [1, 0], [0, -1], [0, 1]].freeze
+
+    module Movement
+      def go(direction)
+        v = DIRECTIONS[direction]
+        [first + v.first, last + v.last].extend Movement
+      end
+    end
+
+    # Start, NW, NE, NS, WE, WS, ES (reading order, connections)
+    PIPES = 'SJL|-7F'.chars.each_with_index.to_h.freeze
+    PIPE_EXIT = [
+      # N   S   W   E  (enter direction)
+      [],
+      [nil, 2, nil, 0],
+      [nil, 3, 0, nil],
+      [0, 1, nil, nil],
+      [nil, nil, 2, 3],
+      [2, nil, nil, 1],
+      [3, nil, 1, nil]
+    ].freeze
+
+    def initialize(lines)
+      @map = lines.map.with_index do |l, y|
+        l.chars.map.with_index do |c, x|
+          PIPES[c].tap do |p|
+            @start = [y + 1, x + 1].extend Movement if p&.zero?
+          end
+        end
+      end
+    end
+
+    def cell(row, col)
+      return nil if row.negative? || col.negative?
+      @map[row - 1][col - 1]
+    end
+
+    def start_point
+      @start
+    end
+
+    def farthest_distance
+      traverse.first
+    end
+
+    def farthest_point
+      traverse.last
+    end
+
+    # Queue-order floodfill
+    def traverse
+      traversers = 4.times.map { |d| [start_point, d] }
+      distance = 0
+
+      loop do
+        distance += 1
+        positions = traversers.map(&:first)
+
+        traversers.map! do |pos, dir|
+          pnew = pos.go dir
+          pipe = cell(*pnew)
+          next nil unless pipe
+
+          dnew = PIPE_EXIT[pipe][dir]
+          next nil unless dnew
+          next nil if positions.include? pnew
+
+          [pnew, dnew]
+        end
+        traversers.compact!
+        traversers.uniq!(&:first)
+
+        raise StopIteration if traversers.length == 1
+      end
+
+      [distance, traversers.first.first]
+    end
+  end
+end
---
+++ spec/day10/part1_spec.rb
@@ -0,0 +1,68 @@
+# frozen_string_literal: true
+
+require 'day10/part1'
+
+RSpec.describe Day10::Part1, :aggregate_failures do
+  subject(:p1) { described_class.new(example_data.each) }
+
+  context 'with the first example' do
+    with_example_data :day10_1a
+
+    describe '.cell' do
+      example 'it reads cells in row, column order' do
+        expect(p1.cell(2, 2)).to eq 0
+        expect(p1.cell(2, 3)).to eq 4
+        expect(p1.cell(2, 4)).to eq 5
+        expect(p1.cell(3, 4)).to eq 3
+        expect(p1.cell(4, 4)).to eq 1
+        expect(p1.cell(4, 2)).to eq 2
+      end
+    end
+
+    describe '.start_point' do
+      it 'is at row 2, column 2' do
+        expect(p1.start_point).to eq [2, 2]
+      end
+    end
+
+    describe '.farthest_point' do
+      it 'is at row 4, column 4' do
+        expect(p1.farthest_point).to eq [4, 4]
+      end
+    end
+
+    describe '.farthest_distance' do
+      it 'is 4 steps from the start' do
+        expect(p1.farthest_distance).to eq 4
+      end
+    end
+  end
+
+  context 'with the second example' do
+    with_example_data :day10_1b
+
+    describe '.cell' do
+      example 'it reads cells in row, column order' do
+        expect(p1.cell(2, 1)).to eq nil
+      end
+    end
+
+    describe '.start_point' do
+      it 'is at row 3, column 1' do
+        expect(p1.start_point).to eq [3, 1]
+      end
+    end
+
+    describe '.farthest_point' do
+      it 'is at row 3, column 5' do
+        expect(p1.farthest_point).to eq [3, 5]
+      end
+    end
+
+    describe '.farthest_distance' do
+      it 'is 8 steps from the start' do
+        expect(p1.farthest_distance).to eq 8
+      end
+    end
+  end
+end
